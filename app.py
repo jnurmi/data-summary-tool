898ceec48165840be0ca5f69fd4944c4bb77be9c
jnurmi <joonas.nurmi@aalto.fi>
2020-04-20 14:53:09 +0300

    Fixed gitignore

--- app.py
+++ app.py
@@ -5,17 +5,19 @@ GLOBAL_COUNTER = 0
 fake = Faker()
 
 app = Flask(__name__,
-            static_folder = "./dist/static",
-            template_folder = "./dist")
+            static_folder="./dist/static",
+            template_folder="./dist")
+
 
 @app.route('/api/getdata', methods=['GET'])
 def random_word():
     global GLOBAL_COUNTER
     GLOBAL_COUNTER += 1
     return jsonify({
-        'idx': GLOBAL_COUNTER, 
+        'idx': GLOBAL_COUNTER,
         'text': fake.name()
     })
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
